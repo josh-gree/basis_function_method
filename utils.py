df9221c985051c070803ee2796d8d3f38cff2eb1
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2017-06-04 16:13:00 +0100

    wrong fbp call

--- utils.py
+++ utils.py
@@ -21,7 +21,7 @@ angle = odl.uniform_partition(0, 2 * np.pi, Np)
 geometry = odl.tomo.Parallel2dGeometry(angle, detector)
 operator = odl.tomo.RayTransform(
     space, geometry, impl="astra_cuda", use_cache=True)
-fbp = odl.tomo.fbp_op(operator)
+fbp_op = odl.tomo.fbp_op(operator)
 
 
 def fwd(X): return operator(X).asarray()
@@ -83,17 +83,17 @@ if __name__ == '__main__':
     # make data
     phantom = poly_phantom(template_array())
     sino = poly_projection(fwd, phantom, Is)
-    fbp = fbp_full(sino).asarray()
+    fbp = fbp_opsino).asarray()
 
     # gradient
     def grad(X): return gradient(X, Is, sino, Nx, Ny, Np, Nd, Ne, fwd, bwd)
 
     # M
-    materials = [10, 20, 30, 45, 50]
-    material_profile = CS_Energy(np.array(materials), np.array(Es))
+    materials=[10, 20, 30, 45, 50]
+    material_profile=CS_Energy(np.array(materials), np.array(Es))
 
-    M = material_profile
+    M=material_profile
 
-    S0 = np.ones((5, Nx, Ny))
+    S0=np.ones((5, Nx, Ny))
 
-    out = solve(S0, M,  100, 200, 1, 1, fwd, grad, phantom)
+    out=solve(S0, M,  100, 200, 1, 1, fwd, grad, phantom)
